a4cd2cbdf6046013e651a79059a5b8cfc0fb42a2
abhijit <goraiabhijit24@gmail.com>
2026-01-01 12:05:29 +0530

    serverurl

--- client/config.js
+++ client/config.js
@@ -1,9 +1,10 @@
 // config.js
 const CONFIG = {
   
-  apiUrl: "https://campus-eventss-server-6qwc.vercel.app",
+  // apiUrl: "https://campus-eventss-server-6qwc.vercel.app",
+  apiUrl: "https://capmus-events-complete.vercel.app",
 
 
-//   apiUrl: "http://localhost:3000",
+  // apiUrl: "http://localhost:3000",
 
 };
